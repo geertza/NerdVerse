46a361d802bc8522ea5213c59912bebd9a5776bc
geertza <geertza@gmail.com>
2020-04-24 16:11:53 -0600

    ready to start game

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 let playerName=""
-    br= document.createElement("BR")
+  
 
 
 
         // enter name and start game
-    $(".add-player").click(function(){
+    $(".add-player").click(start)
+    function start(){
    playerName=$(".name").val()
    $(".askName").hide()
    $("#speech1").text("Welcome "+playerName);
     $("#speech2").text("Your transport is a bit late, but shall arrive shortly.");
      $("#speech3").text("While we wait why don't you go over the options.. ");
-    setTimeout(options,2500)
-})
+    setTimeout(options,2000)
+}
 
 //  Give player options for the game
 function options(){
     $("#speech4").text("It defies my logic circuits how this guy is always late!");
-   setTimeout(hesHere,3000)
+    $(".content").hide()
+    setTimeout(tardis,2500)
+    
 }
+ function tardis(){
+     $(".display").prepend('<img id="policeBox" src="./images/tardis.png" >')
+      setTimeout(hesHere,2000)  
+    }
+
 function hesHere(){
     $(".talk").empty()
     $("#speech1").text("Finally he is here!")
+    setTimeout(getin,3000)
+}
+
+function getin(){
+    $(".host1").hide();$(".host2").toggle()
+    $("#speech1").text("Get in the box.");
+    $("#speech2").text("I have many great wonders in there.");
+    $("#speech3").text("Why do you look scared?");
+    $("#speech4").text("You just have to answer some questions..");
+    $(".bubble").css('box-shadow', '2px 5px 6px 10px #003b6f');
+    $(".bubble").append(` <button type="submit" class="startGame">Get in the wierd guy's box</button>`)
 }
