63641323cbbdd4c93a9a2c5e88c2c45e6b5570d5
geertza <geertza@gmail.com>
2020-04-24 14:45:16 -0600

    first page finally done except the doctor

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ let playerName=""
 
 
         // enter name and start game
-    $(".submitName").click(function(){
+    $(".add-player").click(function(){
    playerName=$(".name").val()
    $(".askName").hide()
    $("#speech1").text("Welcome "+playerName);
     $("#speech2").text("Your transport is a bit late, but shall arrive shortly.");
-    $("#speech5").hide()
+     $("#speech3").text("While we wait why don't you go over the options.. ");
     setTimeout(options,2500)
 })
 
 //  Give player options for the game
 function options(){
-    $("#speech4").text("While we wait why don't we go over the options.. ");
-    let options = $("<div></div>");
-    
-    options.addClass("options");
-     $(".bubble").append(options);
-     
+    $("#speech4").text("It defies my logic circuits how this guy is always late!");
+   setTimeout(hesHere,3000)
+}
+function hesHere(){
+    $(".talk").empty()
+    $("#speech1").text("Finally he is here!")
 }
